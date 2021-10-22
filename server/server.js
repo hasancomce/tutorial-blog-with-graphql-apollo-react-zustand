5f515f9d16006436fb1da25f28cd0e9e770040dd
Hasan <retiredlawyyer@gmail.com>
2021-10-22 16:08:39 +0300

    typedefs and resolvers edited. posts data added.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,50 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
+require('dotenv').config()
+
+const allPosts = [
+    {
+        id: "1",
+        title: "Post 1 Title",
+        content: "Post 1 Content"
+    },
+    {
+        id: "2",
+        title: "Post 2 Title",
+        content: "Post 2 Content"
+    },
+    {
+        id: "3",
+        title: "Post 3 Title",
+        content: "Post 3 Content"
+    },
+    {
+        id: "4",
+        title: "Post 4 Title",
+        content: "Post 4 Content"
+    },
+]
 
 const typeDefs = gql`
 
+    type Post {
+        id: ID!,
+        title: String!,
+        content: String!
+    }
+
     type Query {
-        firstType: String!
+        GetAllPosts: [Post]
     }
 `;
 
 const resolvers = {
     Query: {
-        firstType: () => {
-            return "String text returned."
+        GetAllPosts() {
+            return allPosts
         }
     }
 }
-
 
 const server = new ApolloServer({
     typeDefs,
