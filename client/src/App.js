4a680a48e5499e6ad5cc726ee6d483a4ac843d54
Hasan <retiredlawyyer@gmail.com>
2021-10-27 00:58:33 +0300

    routers created for different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import PostAdd from './pages/PostAdd';
+import PostDetail from './pages/PostDetail';
+import PostEdit from './pages/PostEdit';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/post-add" exact component={PostAdd} />
+        <Route path="/post-detail" exact component={PostDetail} />
+        <Route path="/post-edit" exact component={PostEdit} />
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
